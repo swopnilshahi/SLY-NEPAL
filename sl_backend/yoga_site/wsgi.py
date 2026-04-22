f63d6f9afab6b2954c155064e67373fac8c8eb36
Swopnil Shahi <swopnilshahi8@gmail.com>
2026-04-22 12:23:03 +0545

    update for cicd

--- sl_backend/yoga_site/wsgi.py
+++ sl_backend/yoga_site/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.insert(0, "/home2/laughter/projects/django-react")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yoga_site.settings')
 
 application = get_wsgi_application()
