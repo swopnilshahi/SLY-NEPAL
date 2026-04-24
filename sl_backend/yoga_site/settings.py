9d4ac2afb0ef31efa3129d6db8ffdf824505b769
Swopnil Shahi <swopnilshahi8@gmail.com>
2026-04-24 12:05:04 +0545

    update to pgsql for server

--- sl_backend/yoga_site/settings.py
+++ sl_backend/yoga_site/settings.py
@@ -79,12 +79,26 @@ WSGI_APPLICATION = 'yoga_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
+    }
+
 
 
 # Password validation
