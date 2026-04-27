408d133c36e3bd7192949ded0c297561383f221c
Swopnil Shahi <swopnilshahi8@gmail.com>
2026-04-27 14:58:58 +0545

    updated api dynamically frontend

--- sl_frontend/src/api.js
+++ sl_frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://localhost:8000/api/",
+// });
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const fetchServices = () => API.get("services/");
